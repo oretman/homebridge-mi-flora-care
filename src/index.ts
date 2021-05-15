edefedfd1970348012733bd67462f18f72a0f372
oretman <72998253+oretman@users.noreply.github.com>
2021-05-16 01:56:44 +0200

    Mi Flora Care implementation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+import { ACCESSORY_NAME } from './settings';
+import { MiFloraCareAccessory } from './miFloraAccessory';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  // api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerAccessory(ACCESSORY_NAME, MiFloraCareAccessory);
 };
